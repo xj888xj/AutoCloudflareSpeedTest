bc7df7d18c87f4dfc45cc6a56427661919f698c8
cmliu.net <admin@cmliu.net>
2023-08-13 03:54:18 +0800

    Update speed.bat

--- speed.bat
+++ speed.bat
@@ -89,23 +89,23 @@ rem 二级域名个数，例如配置5个，则域名分别是cl1、cl2、cl3、
 set record_count=5
 set record_type=A
 
-for /F %%I in ('.\curl\bin\curl.exe --silent http://4.ipw.cn') do set PUBLIC_IP=%%I
+for /F %%I in ('.\curl\curl-8.2.1_5-win64-mingw\bin\curl.exe --silent http://4.ipw.cn') do set PUBLIC_IP=%%I
 echo '请确认该机器没有通过代理，你的IP地址是：'%PUBLIC_IP%
 echo '欢迎关注youtuber小道笔记：https://www.youtube.com/channel/UCfSvDIQ8D_Zz62oAd5mcDDg'
 set /a record_count+=1
 
 CloudflareST.exe -url https://cs.cmliussss.link -p 0
-for /F %%I in ('.\curl\bin\curl.exe -X GET "https://api.cloudflare.com/client/v4/zones?name=%zone_name%" -H "X-Auth-Email: %auth_email%" -H "X-Auth-Key: %auth_key%" -H "Content-Type: application/json"') do set zone_identifier=%%I
+for /F %%I in ('.\curl\curl-8.2.1_5-win64-mingw\bin\curl.exe -X GET "https://api.cloudflare.com/client/v4/zones?name=%zone_name%" -H "X-Auth-Email: %auth_email%" -H "X-Auth-Key: %auth_key%" -H "Content-Type: application/json"') do set zone_identifier=%%I
 echo zone_id:%zone_identifier:~18,32%
 
 set /a n=0
 for /f "tokens=1 delims=," %%i in (result.csv) do (
 	if !n! neq 0 (
-		for /F %%I in ('.\curl\bin\curl.exe -X GET "https://api.cloudflare.com/client/v4/zones/%zone_identifier:~18,32%/dns_records?name=%record_name%!record_count!.%zone_name%" -H "X-Auth-Email: %auth_email%" -H "X-Auth-Key: %auth_key%" -H "Content-Type: application/json"') do set record=%%I
+		for /F %%I in ('.\curl\curl-8.2.1_5-win64-mingw\bin\curl.exe -X GET "https://api.cloudflare.com/client/v4/zones/%zone_identifier:~18,32%/dns_records?name=%record_name%!record_count!.%zone_name%" -H "X-Auth-Email: %auth_email%" -H "X-Auth-Key: %auth_key%" -H "Content-Type: application/json"') do set record=%%I
 		echo record_id:!record:~18,32!
 		::echo "https://api.cloudflare.com/client/v4/zones/%zone_identifier:~18,32%/dns_records/!record:~18,32!" -H "X-Auth-Email: %auth_email%" -H "X-Auth-Key: %auth_key%" -H "Content-Type: application/json" --data "{\"type\":\"%record_type%\",\"name\":\"%record_name%!record_count!.%zone_name%\",\"content\":\"%%i\",\"ttl\":60,\"proxied\":false}"
 		echo 更新DNS记录
-		for /F %%I in ('.\curl\bin\curl.exe -X PUT "https://api.cloudflare.com/client/v4/zones/%zone_identifier:~18,32%/dns_records/!record:~18,32!" -H "X-Auth-Email: %auth_email%" -H "X-Auth-Key: %auth_key%" -H "Content-Type: application/json" --data "{\"type\":\"%record_type%\",\"name\":\"%record_name%!record_count!.%zone_name%\",\"content\":\"%%i\",\"ttl\":60,\"proxied\":false}"') do set result=%%I
+		for /F %%I in ('.\curl\curl-8.2.1_5-win64-mingw\bin\curl.exe -X PUT "https://api.cloudflare.com/client/v4/zones/%zone_identifier:~18,32%/dns_records/!record:~18,32!" -H "X-Auth-Email: %auth_email%" -H "X-Auth-Key: %auth_key%" -H "Content-Type: application/json" --data "{\"type\":\"%record_type%\",\"name\":\"%record_name%!record_count!.%zone_name%\",\"content\":\"%%i\",\"ttl\":60,\"proxied\":false}"') do set result=%%I
 		echo %record_name%!record_count!.%zone_name%域名地址更新为:%%i
     	echo 更新结果：!result:~-41,14!		
 	    )
