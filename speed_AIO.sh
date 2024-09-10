ab85e97c5fd787015f2a9e6ba6f59b8145dd6a1f
renzisen <xj888xj@qq.com>
2024-09-11 02:12:33 +0800

    Update speed_AIO.sh

--- speed_AIO.sh
+++ speed_AIO.sh
@@ -137,14 +137,14 @@ if [ ! -f "/usr/share/GeoIP/GeoLite2-Country.mmdb" ]; then
 	echo "文件 /usr/share/GeoIP/GeoLite2-Country.mmdb 不存在。正在下载..."
 	
 	# 使用curl命令下载文件
-	wget /usr/share/GeoIP/GeoLite2-Country.mmdb "${proxygithub}https://github.com/P3TERX/GeoLite.mmdb/releases/download/$geoiplookup_latest_version/GeoLite2-Country.mmdb"
+	wget -N -P /usr/share/GeoIP/GeoLite2-Country.mmdb "${proxygithub}https://github.com/P3TERX/GeoLite.mmdb/releases/download/$geoiplookup_latest_version/GeoLite2-Country.mmdb"
 	
 	# 检查下载是否成功
 	if [ $? -eq 0 ]; then
 		echo "下载完成。"
-	else
-		echo "下载失败。脚本终止。"
-		exit 0
+#	else
+#		echo "下载失败。脚本终止。"
+#		exit 0
 	fi
 fi
 
