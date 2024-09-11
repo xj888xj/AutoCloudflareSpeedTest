021cd3f2b1705fdd1d7344cff89b9b65c9af7b03
renzisen <xj888xj@qq.com>
2024-09-11 10:04:51 +0800

    Update speed_AIO.sh

--- speed_AIO.sh
+++ speed_AIO.sh
@@ -351,9 +351,9 @@ echo "正在将IP按国家代码保存到ip文件夹内..."
     while read -r line; do
         ip=$(echo $line | cut -d ' ' -f 1)  # 提取IP地址部分
 		
-        country_code=$(geoiplookup $ip | awk -F ', ' '{print $1}')  # 获取国家代码
+        #country_code=$(geoiplookup $ip | awk -F ', ' '{print $1}')  # 获取国家代码
 		
-		mmdblookup --file /usr/share/GeoIP/GeoLite2-Country.mmdb  --ip 8.8.8.8 country iso_code
+		#mmdblookup --file /usr/share/GeoIP/GeoLite2-Country.mmdb  --ip 8.8.8.8 country iso_code
 		result=$(mmdblookup --file /usr/share/GeoIP/GeoLite2-Country.mmdb --ip $ip country iso_code)
 		country_code=$(echo $result | awk -F '"' '{print $2}')
 		sudo echo $ip >> "ip/${country_code}-${port}.txt"  # 写入对应的国家文件
@@ -468,7 +468,7 @@ for identifier in "${record_identifiers[@]}"; do
 done
 speedqueue=$((ips + speedqueue_max)) #自定义测速队列，多测2条做冗余
 
-#./CloudflareST -tp 443 -url "https://cs.xj888xjssss.link" -f "ip/HK.txt" -dn 128 -tl 260 -p 0 -o "log/HK.csv"
+#./CloudflareST -tp 443 -url "https://cs.cmliussss.link" -f "ip/HK.txt" -dn 128 -tl 260 -p 0 -o "log/HK.csv"
 sudo ./CloudflareST -tp $port -url $speedurl -f $ip_txt -dn $speedqueue -tl 280 -tlr $lossmax -p 0 -sl $speedlower -o $result_csv
 
 if [ "$record_count" -gt 0 ]; then
