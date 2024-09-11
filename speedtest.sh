b9bb02842f001ef699fb1c4f02562050fbba4543
renzisen <xj888xj@qq.com>
2024-09-11 12:54:45 +0800

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -164,5 +164,5 @@ fi
 
 #CloudflareST测试
 #./CloudflareST -tp 443 -url "https://cs.cmliussss.link" -f "ip/HK.txt" -dn 128 -tl 260 -p 0 -o "log/HK.csv"
-sudo ./CloudflareST -tp 443 -url https://spurl.api.030101.xyz/50mb -dn 10 -tl 280 -tll 40 -tlr 0 -sl 10 -f ip.txt -o log/result-ip.csv
+sudo ./CloudflareST -tp 443 -url https://speed.cloudflare.com/__down?bytes=10000000 -dn 10 -tl 280 -tll 40 -tlr 0 -sl 10 -f ip.txt -o log/result-ip.csv
 #sudo ./CloudflareST -tp 443 -url https://spurl.api.030101.xyz/50mb -dn 10 -tl 280 -tll 40 -tlr 0 -sl 10 -f ip-443.txt -o log/result-443.csv
